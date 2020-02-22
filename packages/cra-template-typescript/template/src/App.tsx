f2641cf3f13cc1e2a467a8ebcc7d124ea2dfafab
joanzo <jo@joanzo.com>
2020-02-22 22:04:58 +0800

    Add custom boilerplate message

--- packages/cra-template-typescript/template/src/App.tsx
+++ packages/cra-template-typescript/template/src/App.tsx
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getEnvValues = () => {
+    if (!process.env.REACT_APP_EMPLOYEE_ID || !process.env.REACT_APP_POSITION_ID) {
+      throw new Error('Please define `REACT_APP_EMPLOYEE_ID` and `REACT_APP_POSITION_ID` in your .env file');
+    }
+
+    const employeeID = process.env.REACT_APP_EMPLOYEE_ID
+    const position = process.env.REACT_APP_POSITION_ID;
+
+    return { employeeID, position };
+  }
+
+  const { employeeID, position } = getEnvValues();
+
   return (
     <div className="App">
       <header className="App-header">
